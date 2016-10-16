195b08ac66067f3493a16d23d048879d5465f599
dinesh <dineshpy07@gmail.com>
2016-10-16 14:30:07 +0530

    Create a separate function for formatting display text
    format_display function creates a proper display string with the given
    length

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,35 @@ use std::fs;
 use colored::*;
 
 fn main() {
-    println!("Listing files in a directory");
     let files = fs::read_dir("./").unwrap();
+    let files_count = fs::read_dir("./").unwrap().count();
     let mut file_count = 0;
+
+    println!("total  {}", files_count);
     for file in files {
         file_count += 1;
         let file_path = file.unwrap().path();
         let mut file_path_display = file_path.display().to_string();
-        let file_display_len = file_path_display.len();
+        let mut file_size = file_path.metadata().unwrap().len().to_string();
+        let file_permissions = file_path.metadata().unwrap().permissions().readonly();
+        let file_modified = file_path.metadata().unwrap().modified().unwrap();
 
-        // Lets take a value of 20
-        let word_space = 20;
-        let actual_space = word_space - file_display_len;
-        for _ in 0..actual_space {
-            file_path_display.push_str(" ");
-        }
+        format_display(& mut file_path_display, 15);
+        format_display(& mut file_size, 6);
 
-        println!("{}   {}   {:?}", file_count.to_string().green(),
+        println!("{}   {}   {}   {:?}   {:?}", file_count.to_string().green(),
          file_path_display.yellow().bold(),
-         file_path.metadata().unwrap().len()
+         file_size,
+         file_permissions,
+         file_modified
         )
+    }
+}
+
+fn format_display(text: & mut String, value: usize) {
+    let actual_size = value - text.len();
+
+    for _ in 0..actual_size {
+        text.push_str(" ");
     }
 }
