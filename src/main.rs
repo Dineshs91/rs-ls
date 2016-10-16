1d014eb78d4075cc86d10b3c5ee220d348920ad7
dinesh <dineshpy07@gmail.com>
2016-10-17 01:41:43 +0530

    Show the time the file was modified in local timezone

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
 extern crate colored;
+extern crate chrono;
 
 use std::fs;
+use std::os::unix::fs::MetadataExt;
+use std::time::UNIX_EPOCH;
 use colored::*;
+use chrono::*;
 
 fn main() {
     let files = fs::read_dir("./").unwrap();
-    let files_count = fs::read_dir("./").unwrap().count();
+    let files_block = fs::read_dir("./").unwrap();
     let mut file_count = 0;
+    let mut file_block_count = 0;
 
-    println!("total  {}", files_count);
+    for f in files_block {
+        file_block_count  += f.unwrap().path().metadata().unwrap().blocks();
+    }
+
+    println!("total  {}", file_block_count);
 
     for file in files {
         file_count += 1;
@@ -17,17 +26,16 @@ fn main() {
         let mut file_path_display = file_path.display().to_string();
         let mut file_size = file_path.metadata().unwrap().len().to_string();
 
-        let file_permissions = file_path.metadata().unwrap().permissions().readonly();
-        let file_modified = file_path.metadata().unwrap().modified().unwrap();
+        let file_modified = file_path.metadata().unwrap().modified().unwrap().duration_since(UNIX_EPOCH).unwrap().as_secs();
+        let dt = Local.timestamp(file_modified as i64, 0);
 
         format_display(& mut file_path_display, 15);
         format_display(& mut file_size, 6);
 
-        println!("{}   {}   {}   {:?}   {:?}", file_count.to_string().green(),
+        println!("{}   {}  {}  {}", file_count.to_string().green(),
            file_path_display.yellow().bold(),
            file_size,
-           file_permissions,
-           file_modified
+           dt.format("%h %d %H:%M")
         )
     }
 }
